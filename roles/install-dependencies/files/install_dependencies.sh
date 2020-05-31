fca6ad1e62fbb498b286538bfa632e1deede267d
Jaynti Kanani <jdkanani@gmail.com>
2020-05-31 22:37:47 +0530

    new: initial setup

---
+++ roles/install-dependencies/files/install_dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl https://gist.githubusercontent.com/jdkanani/e18e14910652ad829fad994e4b89f0b9/raw/77b3cb2541e3ebe8d9ae3427a6f8a52d7f310b6f/go-install.sh > install_go.sh
+bash /home/ubuntu/install_go.sh

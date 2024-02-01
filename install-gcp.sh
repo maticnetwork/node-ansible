fde97ff8b50bf61a0e7d48d0a76c9fc067ddfaaa
Piyush Bagani <piyush.bagani@searce.com>
2024-02-01 22:31:40 +0530

    updated marketplace pos node startup-script

--- install-gcp.sh
+++ install-gcp.sh
@@ -16,6 +16,7 @@ function check_os() {
     exit 1
   fi
 }
+
 function mount_disk() {
   EXT_DISK="${1}"
   MOUNT_POINT="${2}"
@@ -38,6 +39,7 @@ function mount_disk() {
   # restore content if any
   cp -a "${MOUNT_POINT}.old/." "${MOUNT_POINT}/"
 }
+
 function apt_upd() {
   apt update
 }
@@ -45,12 +47,12 @@ function apt_upd() {
 function install_dependencies() {
   apt_upd
   apt install -f git
+  apt-get update -y
+  apt-get install -y zstd pv aria2
 }
 
 function install_ansible() {
   apt_upd
-#  apt install -y software-properties-common
-#  add-apt-repository --yes --update ppa:ansible/ansible
   apt install -y ansible
 }
 
@@ -72,66 +74,91 @@ function configure_inventory() {
 
 function configure_node() {
   # https://docs.polygon.technology/docs/validate/validate/run-validator-ansible#configure-the-heimdall-service
-  http_port="${1}"
-  network="${2}"
-  HEIMDALLD_CONFIG="/root/.heimdalld/config/config.toml"
-  BOR_START_SH="/root/node/bor/start.sh"
-  if [ "${network}" == "mainnet" ];then
-    sed -i 's/^seeds =.*/seeds ="f4f605d60b8ffaaf15240564e58a81103510631c@159.203.9.164:26656,4fb1bc820088764a564d4f66bba1963d47d82329@44.232.55.71:26656,2eadba4be3ce47ac8db0a3538cb923b57b41c927@35.199.4.13:26656,3b23b20017a6f348d329c102ddc0088f0a10a444@35.221.13.28:26656,25f5f65a09c56e9f1d2d90618aa70cd358aa68da@35.230.116.151:26656"/g' "${HEIMDALLD_CONFIG}"
+
+  option="${1}"
+  BOR_CONFIG="/var/lib/bor/config.toml"
+
+  if [ "${option}" == "ws" ]; then
+    # comment out [jsonrpc.http] section
+    sed -i '/\[jsonrpc\.http\]/,/corsdomain = \["\*"\]/ s/^/#/' "$BOR_CONFIG"
+    
+    # uncomment [jsonrpc.ws] section
+    sed -i '/\[jsonrpc\.ws\]/,/origins = \["\*"\]/ s/^\(\s*\)#\s\{0,1\}/\1/' "$BOR_CONFIG"
   fi
-  sed -i 's/^prometheus =.*/prometheus = true/g' "${HEIMDALLD_CONFIG}"
-  sed -i 's/^max_open_connections =.*/max_open_connections = 100/g' "${HEIMDALLD_CONFIG}"
-  if ! grep -q bootnodes "${BOR_START_SH}"; then
-    echo '  --bootnodes "enode://0cb82b395094ee4a2915e9714894627de9ed8498fb881cec6db7c65e8b9a5bd7f2f25cc84e71e89d0947e51c76e85d0847de848c7782b13c0255247a6758178c@44.232.55.71:30303,enode://88116f4295f5a31538ae409e4d44ad40d22e44ee9342869e7d68bdec55b0f83c1530355ce8b41fbec0928a7d75a5745d528450d30aec92066ab6ba1ee351d710@159.203.9.164:30303,enode://3178257cd1e1ab8f95eeb7cc45e28b6047a0432b2f9412cff1db9bb31426eac30edeb81fedc30b7cd3059f0902b5350f75d1b376d2c632e1b375af0553813e6f@35.221.13.28:30303,enode://16d9a28eadbd247a09ff53b7b1f22231f6deaf10b86d4b23924023aea49bfdd51465b36d79d29be46a5497a96151a1a1ea448f8a8666266284e004306b2afb6e@35.199.4.13:30303,enode://ef271e1c28382daa6ac2d1006dd1924356cfd843dbe88a7397d53396e0741ca1a8da0a113913dee52d9071f0ad8d39e3ce87aa81ebc190776432ee7ddc9d9470@35.230.116.151:30303"' >> "${BOR_START_SH}"
+
+  if [ "${option}" == "rpc-ws" ]; then
+    # uncomment [jsonrpc.ws] section
+    sed -i '/\[jsonrpc\.ws\]/,/origins = \["\*"\]/ s/^\(\s*\)#\s\{0,1\}/\1/' "$BOR_CONFIG"
   fi
-  sed -i 's/--http.port.*/--http.port '${http_port}' \\/g' "${BOR_START_SH}"
 }
 
 function sentry_node_setup() {
   extra_var=${1}
   cd ${INSTALL_DIR}
+
   ansible-playbook --connection=local -l sentry playbooks/network.yml --extra-var="${extra_var}"
 }
 
 function get_snapshot_url() {
   network="${1}"
-  mode="${2}"
-  # use pruned snapshot instead of fullnode snapshot with mainnet
-  if [ "$mode" == "fullnode" ] && [ "$network" == "mainnet" ] ;then
-    mode="pruned"
-  fi
-  node_type="${3}"
-  curl -s https://snapshots.matic.today/ | grep "${network}/${node_type}-${mode}" | cut -f 3 -d '>' | cut -f 1 -d '<'
+  client="${2}"
+  echo "https://snapshot-download.polygon.technology/$client-$network-parts.txt"
+}
+
+function extract_files() {
+  compiled_files=$1
+  while read -r line; do
+      if [[ "$line" == checksum* ]]; then
+          continue
+      fi
+      filename=`echo $line | awk -F/ '{print $NF}'`
+      if echo "$filename" | grep -q "bulk"; then
+          pv $filename | tar -I zstd -xf - -C . && rm $filename
+      else
+          pv $filename | tar -I zstd -xf - -C . --strip-components=3 && rm $filename
+      fi
+  done < $compiled_files
 }
 
 function load_snapshots() {
   network="${1}"
-  mode="${2}"
-
-  heimdall_snapshot_url=`get_snapshot_url ${network} "" heimdall`
-  bor_snapshot_url=`get_snapshot_url ${network} ${mode} bor`
+  heimdall_snapshot_url=`get_snapshot_url $network heimdall`
+  bor_snapshot_url=`get_snapshot_url $network bor`
 
   echo "$heimdall_snapshot_url"
   echo "$bor_snapshot_url"
 
-  wget "${heimdall_snapshot_url}" -O - | tar -xz -C ~/.heimdalld/data/
-  wget "${bor_snapshot_url}" -O - | tar -xz -C ~/.bor/data/bor/chaindata
-  chown -R root:root ~/.heimdalld/data/ ~/.bor/data/bor/chaindata
+  cd /var/lib/heimdall/data/
+  aria2c -x6 -s6 "${heimdall_snapshot_url}"
+  aria2c -x6 -s6 -i heimdall-$network-parts.txt
+  
+  # execute final data extraction step
+  extract_files heimdall-$network-parts.txt
+
+  cd /var/lib/bor/data/bor/chaindata
+  aria2c -x6 -s6 "${bor_snapshot_url}"
+  aria2c -x6 -s6 -i bor-$network-parts.txt
+
+  # execute final data extraction step
+  extract_files bor-$network-parts.txt
+  
+  chown -R bor:nogroup  /var/lib/bor
+  chown -R heimdall:nogroup  /var/lib/heimdall
 }
 
 function polygonctl() {
   service heimdalld $1
   service heimdalld-rest-server $1
+  service heimdalld-bridge $1
   service bor $1
 }
 
 function usage() {
-  echo "Usage: $0 {-n <mainnet|mumbai> -m <archive|fullnode> -s <snapshot|from_scratch> -p <rpc-port-number>}" 1>&2
+  echo "Usage: $0 {-n <mainnet|mumbai> -m <archive|fullnode> -s <snapshot|from_scratch> -t <ws|rpc> -p <port-number>}" 1>&2
   exit 1
 }
 
-# Getopts
-while getopts ":s:m:n:p:e:" o; do
+while getopts ":s:m:n:p:e:t:" o; do
     case "${o}" in
         s)
             s=${OPTARG}
@@ -148,6 +175,9 @@ while getopts ":s:m:n:p:e:" o; do
         e)
             e=${OPTARG}
             ;;
+        t)
+            t=${OPTARG}
+            ;;
         *)
             usage
             ;;
@@ -155,8 +185,8 @@ while getopts ":s:m:n:p:e:" o; do
 done
 shift $((OPTIND-1))
 
-# Validation
-if [ -z "${s}" ] || [ -z "${m}" ] || [ -z "${n}" ]|| [ -z "${p}" ]; then
+
+if [ -z "${s}" ] || [ -z "${m}" ] || [ -z "${n}" ]|| [ -z "${t}" ]|| [ -z "${p}" ]; then
   usage
 fi
 if [ "${s}" != "snapshot" ] && [ "${s}" != "from_scratch" ]; then
@@ -169,9 +199,10 @@ if [ "${n}" != "mainnet" ] && [ "${n}" != "mumbai" ]; then
   usage
 fi
 if [ -z "${e}" ];then
-  e="bor_branch=v0.2.14 heimdall_branch=v0.2.5  network_version=mainnet-v1 node_type=sentry/sentry heimdall_network=mainnet"
+  e="bor_version=v1.0.0 heimdall_version=v1.0.3 network=mainnet node_type=sentry"
 fi
-echo "EXTRA_VAR=${e}"
+
+echo "EXTRA_VAR=${e}" > /var/log/extra-var.txt
 # p is not a natural decimal number
 if [[ ! $p =~ ^[1-9][0-9]*$ ]];then
   usage
@@ -180,20 +211,24 @@ fi
 if ((p > 65535 || p < 1));then
   usage
 fi
-# Main
+
 check_root
 check_os
 install_dependencies
 install_ansible
 
 clone_repo
+
 configure_inventory
 
 sentry_node_setup "${e}"
-configure_node "${p}" "${n}"
-# we need to mount disks on late phase to prevent "existing datadir" complains from ansible
-mount_disk /dev/sdb /root/.bor/data/bor/chaindata bor
-mount_disk /dev/sdc /root/.heimdalld/data heimdalld
+configure_node "${t}"
+
+mount_disk /dev/sdb /var/lib/bor/data/bor/chaindata bor
+mount_disk /dev/sdc /var/lib/heimdall/data heimdalld
+
+chown -R bor:nogroup  /var/lib/bor
+chown -R heimdall:nogroup  /var/lib/heimdall
 
 if [ "${s}" == "snapshot" ]; then
   load_snapshots ${n} ${m}
